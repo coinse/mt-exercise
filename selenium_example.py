e14985bfeb3324fe2375697ebc94f52cde82ada9
ntrolls <shin.yoo@kaist.ac.kr>
2021-07-09 10:24:33 +0900

    MRSpecific selenium test case

--- selenium_example.py
+++ selenium_example.py
@@ -9,6 +9,5 @@ elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-
-assert "No results found." not in driver.page_source
+# assert "No results found." not in driver.page_source
 driver.close()
--- test_mpspecific.py
+++ test_mpspecific.py
@@ -2,10 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def test_mpspecific():
-    x, y = 0
+    x = 0 
+    y = 0
     p = "kaist"
     q = "kaist computer science"
 
-    # ...
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys(p)
+    elem.send_keys(Keys.RETURN)
+
+    # read the number of returned pages
+    stats = driver.find_element_by_id("result-stats")
+    tokens = stats.text.split(" ")
+    x = int(tokens[2][:-1].replace(",", ""))
+
+    driver.get("https://www.google.com")
+
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys(q)
+    elem.send_keys(Keys.RETURN)
+
+    # read the number of returned pages
+    stats = driver.find_element_by_id("result-stats")
+    tokens = stats.text.split(" ")
+    y = int(tokens[2][:-1].replace(",", ""))
+    driver.close()
 
     assert x >= y
+
+if __name__ == '__main__':
+    test_mpspecific()
